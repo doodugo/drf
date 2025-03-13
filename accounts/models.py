379c7464d4b91b72efb34f6b190901f537b964c4
chlendyd7 <chlendyd7@naver.com>
2025-03-13 21:34:53 +0900

    feat: User model 추가

---
+++ accounts/models.py
@@ -0,0 +1,18 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    USER_TYPE_CHOICES = [
+        ('seller', '판매자'),
+        ('buyer', '구매자'),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True, blank=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['email','username', 'user_type', 'password']
+
+    def __str__(self):
+        return f'{self.username} ({self.user_type})'
