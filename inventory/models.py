7793ddd19c96ece266529235817a671d517e4300
chlendyd7 <chlendyd7@naver.com>
2025-03-13 22:11:10 +0900

    feat: photocard 테이블 구현, 마이그레이션 진행

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,26 @@
+from conf.models import TimeStampedModel
 from django.db import models
 
-# Create your models here.
+
+class PhotoCard(TimeStampedModel):
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        help_text='포토카드 명'
+    )
+
+    artist_name = models.CharField(
+        max_length=30,
+        null=False,
+        help_text='아티스트 명'
+    )
+
+    group_name = models.CharField(
+        max_length=30,
+        null=False,
+        default='',
+        help_text='아티스트 그룹명'
+    )
+
+    def __str__(self):
+        return f"{self.name} - {self.artist_name}"
