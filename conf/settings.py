e32580965ac10239b2c6c4f07c9bf12704cea6ed
chlendyd7 <chlendyd7@naver.com>
2025-03-14 22:23:18 +0900

    feat: rest_framework와 jwt를 사용하기 위해 setting 파일 업데이트

--- conf/settings.py
+++ conf/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -45,8 +46,12 @@ SECOND_APPS = [
     'inventory',
 ]
 
+THIRD_APPS = [
+    'rest_framework',
+    'rest_framework_simplejwt',
+]
 
-INSTALLED_APPS += SECOND_APPS
+INSTALLED_APPS += SECOND_APPS + THIRD_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -59,6 +64,20 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'conf.urls'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
 
 TEMPLATES = [
     {
