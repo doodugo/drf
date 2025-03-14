ae09c134bc7a2e02272c58c5ff5b8ea64eb294c3
chlendyd7 <chlendyd7@naver.com>
2025-03-14 22:15:22 +0900

    feat: 판매 테이블 및 property 추가 , 마이그레이션 진행

--- transactions/models.py
+++ transactions/models.py
@@ -22,3 +22,55 @@ class CashLog(TimeStampedModel):
 
     def __str__(self):
         return f"User {self.user_id.username}: {self.cash}원 ({self.created_date})"
+
+
+class Sale(TimeStampedModel):
+    user_id = models.ForeignKey(
+        to='accounts.User',
+        on_delete=models.DO_NOTHING,
+        related_name='sales',
+        help_text = '사용자 ID',
+        null=False,
+        blank=False,
+    )
+
+    photo_card_id = models.ForeignKey(
+        to='inventory.PhotoCard',
+        on_delete=models.DO_NOTHING,
+        related_name='sales',
+        help_text = '포토카드 ID',
+        null=False,
+        blank=False,
+    )
+
+    amount = models.IntegerField(
+        help_text = '등록 수량',
+        null=False,
+        blank=False,
+    )
+
+    price = models.PositiveIntegerField(
+        help_text = '장 당 가격',
+        null=False,
+        blank=False,
+    )
+
+    def __str__(self):
+        return f" \
+            {self.user_id.username}: {self.photo_card_id.name} \
+            - {self.amount}장 ({self.price}원)"
+
+    @classmethod
+    def purchase_commission(cls, price):
+        if price < 15000:
+            return 0.062
+        return 0.137
+
+    @property
+    def buy_price(self):
+        return self.price + int(self.price * self.purchase_commission(self.price))
+
+    def save(self, *args, **kwargs):
+        if not self.user_id.is_seller:
+            raise ValueError("판매자만 판매 가능합니다.")
+        super().save(*args, **kwargs)
