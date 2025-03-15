ecbac92234390ebcf2403f25f1b3bb71589f6717
chlendyd7 <chlendyd7@naver.com>
2025-03-15 12:08:15 +0900

    feat: jwt를 이용해서 로그인 가능

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import RegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 urlpatterns = [
     path('signup/', RegisterView.as_view(), name='signup'),
+    path('signin/', TokenObtainPairView.as_view(), name='signin'),
 ]
