adf675e049aea182ad382d49dbb0824992a0d59f
chlendyd7 <chlendyd7@naver.com>
2025-03-16 22:03:25 +0900

    Teat: 구매 로직 test SetUp

--- transactions/tests.py
+++ transactions/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from accounts.models import User
+from inventory.models import PhotoCard
+from transactions.models import Sale
+class BuyTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        self.user = User.objects.create_user(
+            email='test_buyer@test.com',
+            username='testuser',
+            password='testpassword',
+            user_type='buyer',
+        )
+
+
+        seller = User.objects.create_user(
+            email='test_seller@test.com',
+            username='test_seller',
+            password='test_seller_password',
+            user_type='seller',
+        )
+
+        photo_card = PhotoCard.objects.create(
+            name='test_photo_card',
+            artist_name='test_artist_name',
+            group_name='test_group_name',
+        )
+        self.sale = Sale.objects.create(
+            user_id=seller,
+            photo_card_id=photo_card,
+            amount=10,
+            price=20000,
+        )
+
+
+
